5b52991e4f4b3e62e488481f9e986b03c87a57e1
Scott B <djlethal109@gmail.com>
2022-05-29 09:28:42 +1000

    now with search delay

--- background.js
+++ background.js
@@ -2,45 +2,56 @@ const SEARCH_URL = `http://api.are.na/v2/search/channels`;
 
 // Provide help text to the user.
 browser.omnibox.setDefaultSuggestion({
-  description: `Search your are.na channels
+  description: `Search are.na channels
     (e.g. "my first channel")`
 });
 
-// Update the suggestions whenever the input is changed.
-browser.omnibox.onInputChanged.addListener((text, addSuggestions) => {
-  console.log(`Receiving input: ${text}`);
+let timeout = 0;
+let searchDelay = 200;
 
-  let headers = new Headers({ "Accept": "application/json" });
-  let init = { method: 'GET', headers };
-  let request = new Request(`${SEARCH_URL}?q=${text}`, init);
+let searchChannels = (text, addSuggestions) => {
+  if (timeout) clearTimeout(timeout);
+
+  timeout = setTimeout(() => {
+    let headers = new Headers({ "Accept": "application/json" });
+    let init = { method: 'GET', headers };
+    let request = new Request(`${SEARCH_URL}?q=${text}`, init);
 
 
-  let suggestions = [];
-  let suggestionsOnEmptyResults = [{
-    content: SEARCH_URL,
-    description: "no results found"
-  }];
+    let suggestions = [];
+    let suggestionsOnEmptyResults = [{
+      content: "https://are.na",
+      description: "No channels found"
+    }];
 
-  fetch(request).then((response) => {
-    response.json().then((json) => {
+    fetch(request).then((response) => {
+      response.json().then((json) => {
 
-      console.log(json);
+        console.log(json);
 
-      json.channels.forEach((channel) => {
-        suggestions.push({
-          content: `https://are.na/${channel.user.slug}/${channel.slug}`,
-          description: `${channel.title}`
+        if (!json.length) {
+          addSuggestions(suggestionsOnEmptyResults);
+        }
+
+        json.channels.forEach((channel) => {
+          suggestions.push({
+            content: `https://are.na/${channel.user.slug}/${channel.slug}`,
+            description: `${channel.title}`
+          })
         })
+
+        addSuggestions(suggestions);
+
       })
-
-      addSuggestions(suggestions);
-
     })
+  }, searchDelay)
 
-  })
+  addSuggestions();
+}
+
+browser.omnibox.onInputChanged.addListener(searchChannels);
 
 
-});
 
 // Open the page based on how the user clicks on a suggestion.
 browser.omnibox.onInputEntered.addListener((text, disposition) => {
