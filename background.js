407b2e9fbca56a114c66eafbcfb6c9f3cbb8193e
Scott B <djlethal109@gmail.com>
2022-05-28 21:13:14 +1000

    working prototype

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
-const BASE_URL = "http://api.are.na/v2/";
-const SEARCH_URL = `${BASE_URL}/search/channels`;
+const SEARCH_URL = `http://api.are.na/v2/search/channels`;
 
 // Provide help text to the user.
 browser.omnibox.setDefaultSuggestion({
@@ -9,87 +8,52 @@ browser.omnibox.setDefaultSuggestion({
 
 // Update the suggestions whenever the input is changed.
 browser.omnibox.onInputChanged.addListener((text, addSuggestions) => {
-  let headers = new Headers({"Accept": "application/json"});
-  let init = {method: 'GET', headers};
-  let url = buildSearchURL(text);
-  let request = new Request(url, init);
+  console.log(`Receiving input: ${text}`);
 
-  fetch(request)
-    .then(createSuggestionsFromResponse)
-    .then(addSuggestions);
+  let headers = new Headers({ "Accept": "application/json" });
+  let init = { method: 'GET', headers };
+  let request = new Request(`${SEARCH_URL}?q=${text}`, init);
+
+
+  let suggestions = [];
+  let suggestionsOnEmptyResults = [{
+    content: SEARCH_URL,
+    description: "no results found"
+  }];
+
+  fetch(request).then((response) => {
+    response.json().then((json) => {
+
+      console.log(json);
+
+      json.channels.forEach((channel) => {
+        suggestions.push({
+          content: `https://are.na/${channel.user.slug}/${channel.slug}`,
+          description: `${channel.title}`
+        })
+      })
+
+      addSuggestions(suggestions);
+
+    })
+
+  })
+
+
 });
 
 // Open the page based on how the user clicks on a suggestion.
 browser.omnibox.onInputEntered.addListener((text, disposition) => {
   let url = text;
-  if (!text.startsWith(SOURCE_URL)) {
-    // Update the url if the user clicks on the default suggestion.
-    url = `${SEARCH_URL}?q=${text}`;
-  }
   switch (disposition) {
     case "currentTab":
-      browser.tabs.update({url});
+      browser.tabs.update({ url });
       break;
     case "newForegroundTab":
-      browser.tabs.create({url});
+      browser.tabs.create({ url });
       break;
     case "newBackgroundTab":
-      browser.tabs.create({url, active: false});
+      browser.tabs.create({ url, active: false });
       break;
   }
 });
-
-function buildSearchURL(text) {
-  let queryParts = [];
-  let query = '';
-  let parts = text.split(' ');
-
-  parts.forEach(part => {
-      queryParts.push(part);
-  });
-
-  query = queryParts.join(' ');
-  return `${SEARCH_URL}?q=${query}`;
-}
-
-function createSuggestionsFromResponse(response) {
-  return new Promise(resolve => {
-    let suggestions = [];
-    let suggestionsOnEmptyResults = [{
-      content: SOURCE_URL,
-      description: "no results found"
-    }];
-    response.json().then(json => {
-      if (!json.normal) {
-        return resolve(suggestionsOnEmptyResults);
-      }
-
-      let occurrences = json.normal["Textual Occurrences"];
-      let files = json.normal["Files"];
-
-      if (!occurrences && !files) {
-        return resolve(suggestionsOnEmptyResults);
-      }
-
-      if (occurrences) {
-        occurrences.forEach(({path, lines}) => {
-          suggestions.push({
-            content: `${SOURCE_URL}/${path}#${lines[0].lno}`,
-            description: lines[0].line,
-          });
-        });
-        return resolve(suggestions);
-      }
-
-      // There won't be any textual occurrences if the "path:" prefix is used.
-      files.forEach(({path}) => {
-        suggestions.push({
-          content: `${SOURCE_URL}/${path}`,
-          description: path,
-        });
-      });
-      return resolve(suggestions);
-    });
-  });
-}
-
